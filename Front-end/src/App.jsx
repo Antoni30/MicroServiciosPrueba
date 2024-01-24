abd48bd065736fa99511da6b56be2e120831165f
Cristian Tello <cristian.andres.t98@gmail.com>
2024-01-23 22:18:09 -0500

    Get request trought the fronted completed

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import AlumnoList from './components/AlumnoList';
+import MateriaList from './components/MateriaList';
+import TareaList from './components/TareaList';
 import './App.css'
 
+const ENDPOINT = 'http://localhost:2024';
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [setAlumnos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const AlumnosResponse = await fetch('{ENDPOINT}/api/alumnos');
+        const jsonData = await AlumnosResponse.json();
+        setAlumnos(jsonData);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+    const socket = io(ENDPOINT);
+    socket.on('newAlumno', (newAlumno) => {
+      setAlumnos((prevAlumno) => [...prevAlumno, newAlumno]);
+    });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='grid grid-rows-4 grid-flow-col gap-4'>
+      <div className="col">
+        <h1>Lista de Alumnos</h1>
+        <AlumnoList />        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="col">
+        <h1>Lista de Materias</h1>
+        <MateriaList />       
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="flex flex-col items-center justify-center">
+        <h1>Lista de Tareas</h1>
+          <TareaList />  
+      </div>
+
+    </div>
   )
 }
 
